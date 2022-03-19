d9f2389470993ae1345f3519435344e7eaeb2ff6
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-03-19 18:07:13 +0100

    organize esdoc

---
+++ esdoc/src/components.ts
@@ -0,0 +1,94 @@
+import { renderToString } from "katex";
+
+export interface EsComponent {
+    toHtml(): string;
+    toJson(): string;
+    toMarkdown(): string;
+}
+
+export class EsText implements EsComponent {
+    constructor(public text: string) { }
+
+    public toHtml(): string {
+        const text = this.text
+            .trim()
+            .replace(/\n[ \t]*\n/, '<br>')
+            .replace(/\s+/g, ' ')
+        return /*html*/ `<p class="escomponent estext">${text}</p>`;
+    }
+
+    public toJson(): string {
+        return `{"type":"text","text":"${this.text.replace(/\n/g, '\\n')}"}`;
+    }
+
+    public toMarkdown(): string {
+        return this.text;
+    }
+}
+
+export class EsTitle implements EsComponent {
+    constructor(public text: string) { }
+
+    public toHtml(): string {
+        return /*html*/ `<h2 class="escomponent estitle">${this.text}</h2>`;
+    }
+
+    public toJson(): string {
+        return `{"type":"title","text":"${this.text}"}`;
+    }
+
+    public toMarkdown(): string {
+        return `## ${this.text}`;
+    }
+}
+
+export class EsImage implements EsComponent {
+    constructor(public src: string, public alt: string) { }
+
+    public toHtml(): string {
+        return /*html*/ `<img class="escomponent esimage" src="${this.src}" alt="${this.alt}">`;
+    }
+
+    public toJson(): string {
+        return `{"type":"image","src":"${this.src}","alt":"${this.alt}"}`;
+    }
+
+    public toMarkdown(): string {
+        return `![${this.alt}](${this.src})`;
+    }
+}
+
+export class EsMath implements EsComponent {
+    constructor(public latex: string) { }
+
+    public toHtml(): string {
+        return renderToString(this.latex, { throwOnError: true })
+            .replace('<span class="katex">', '<span class="escomponent esmath katex">');
+    }
+
+    public toJson(): string {
+        return `{"type":"math","latex":"${this.latex.replace(/\\/g, '\\\\')}"}`;
+    }
+
+    public toMarkdown(): string {
+        return this.latex;
+    }
+}
+
+export class EsCode implements EsComponent {
+    constructor(public code: string, public lang?: string) { }
+
+    public toHtml(): string {
+        const langClass = this.lang ? `lang-${this.lang}` : '';
+        return /*html*/ `<code class="escomponent escode ${langClass}"><pre>${this.code}</pre></code>`;
+    }
+
+    public toJson(): string {
+        return `{"type":"code","lang":"${this.lang}","code":${JSON.stringify(this.code)}}`;
+    }
+
+    public toMarkdown(): string {
+        const backticks = '```';
+        return `${backticks}${this.lang ?? ''}\n${this.code.trim()}\n${backticks}`;
+    }
+}
---
+++ esdoc/src/parser.ts
@@ -0,0 +1,99 @@
+import { EsCode, EsComponent, EsImage, EsMath, EsText, EsTitle } from "./components";
+import { EsDocument } from "./document";
+
+export const enum EsParserStates {
+    TEXT,
+    DIRECTIVE,
+}
+
+export class EsParser {
+    private state: EsParserStates = EsParserStates.TEXT;
+    private components: EsComponent[] = [];
+    private lastWord = '';
+    private lastCharWasWordChar = false;
+    private quoteDepth = 0;
+    private textBuffer = '';
+
+    constructor(public text: string) { }
+
+    public parse(): EsDocument {
+        this.components = [];
+        for (const char of this.text)
+            this.parseChar(char);
+        this.endTextComponent();
+        return new EsDocument(this.components);
+    }
+
+    private parseChar(char: string) {
+        switch (this.state) {
+            case EsParserStates.TEXT:
+                return this.walkText(char);
+            case EsParserStates.DIRECTIVE:
+                return this.walkDirective(char);
+        }
+    }
+
+    private walkText(char: string) {
+        if (/[a-zA-Z]/.test(char)) {
+            if (!this.lastCharWasWordChar)
+                this.lastWord = '';
+            this.lastWord += char;
+            this.lastCharWasWordChar = true;
+        } else {
+            this.lastCharWasWordChar = false;
+        }
+        if (char == '"')
+            this.quoteDepth++;
+        else
+            this.quoteDepth = 0;
+        if (this.quoteDepth === 2)
+            return this.endTextComponent();
+        else
+            this.textBuffer += char;
+    }
+
+    private endTextComponent() {
+        this.components.push(new EsText(
+            this.textBuffer
+                .replace(new RegExp(`${this.lastWord}\\s*"$`), '')
+        ));
+        this.textBuffer = '';
+        this.quoteDepth = 0;
+        this.state = EsParserStates.DIRECTIVE;
+    }
+
+    private walkDirective(char: string) {
+        if (char == '"') 
+            this.quoteDepth++;
+        else
+            this.quoteDepth = 0;
+        if (this.quoteDepth === 2)
+            return this.endDirective();
+        else
+            this.textBuffer += char;
+    }
+
+    private endDirective() {
+        this.components.push(this.makeDirective());
+        this.textBuffer = '';
+        this.lastWord = '';
+        this.quoteDepth = 0;
+        this.state = EsParserStates.TEXT;
+    }
+
+    private makeDirective(): EsComponent {
+        const text = this.textBuffer.replace(/"+$/, '');
+        switch (this.lastWord) {
+            case 'title':
+                return new EsTitle(text);
+            case 'image':
+                return new EsImage(text, '');
+            case 'math':
+                return new EsMath(text);
+            case 'code':
+                return new EsCode(text);
+            default:
+                return new EsText(text);
+        }
+    }
+}
