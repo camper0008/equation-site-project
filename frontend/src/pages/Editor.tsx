7470cf21b1c5c80af24a511c9a622266af69df69
camper0008 <spetakel123@gmail.com>
2022-03-22 11:01:19 +0100

    previewing with esdoc

--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -1,18 +1,32 @@
 import { Logo } from "../components/Logo";
 import { StateManager } from "../StateManager";
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
+import { EsParser } from "esdoc";
 import "../assets/editor.scss";
 
 const Editor: Component<Props> = () => {
+    const [previewHtml, setPreviewHtml] = createSignal(
+        "<p>Her vises der noget tekst.</p>",
+    );
+
+    const parse = () => {
+        const editor = document.getElementById("editor") as HTMLInputElement;
+
+        const doc = new EsParser(editor.value).parse();
+        setPreviewHtml(doc.toHtml());
+    };
+
     return (
         <div id="equation-editor-container">
             <div id="editor-container">
                 <h2>Redigering</h2>
-                <textarea id="editor"></textarea>
+                <textarea id="editor" onInput={parse}>
+                    Her vises der noget tekst.
+                </textarea>
             </div>
             <div id="preview-container">
                 <h2>Forhåndsvisning</h2>
-                <div id="preview"></div>
+                <div id="preview" innerHTML={previewHtml()}></div>
             </div>
         </div>
     );
