1c6ac3876d97124859219b4c6fbe84276c14191b
camper0008 <spetakel123@gmail.com>
2022-03-23 22:14:26 +0100

    dotenv

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,8 @@
 use crate::database::mongo_db::MongoDb;
 use actix_cors::Cors;
 use actix_web::{web::Data, App, HttpServer};
-use std::sync::Mutex;
+use dotenv;
+use std::{env, sync::Mutex};
 
 mod database;
 mod models;
@@ -13,11 +14,10 @@ use crate::routes::users;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let db = MongoDb::new(
-        "mongodb://localhost:27017/".to_string(),
-        "equation-site-project".to_string(),
-    )
-    .await;
+    dotenv::dotenv().expect("unable to load .env file");
+    let uri = env::var("MONGO_URI").expect("unable to get MONGO_URI environment variable");
+
+    let db = MongoDb::new(uri.to_string(), "equation-site-project".to_string()).await;
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(Mutex::new(db.clone())))
